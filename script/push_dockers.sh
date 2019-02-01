1052e501f6c665e0114f337ab7095128ee0337a4
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-01 17:25:10 -0500

    Uses docker run to get chrome version

--- script/push_dockers.sh
+++ script/push_dockers.sh
@@ -3,7 +3,7 @@
 for version in 6.16.0 8.15.0 10.15.1
 do
   BASE_IMAGE=chromium_headless_node:$version
-  CHROMIUM_VERSION=$(docker-compose run $BASE_IMAGE chromium-browser --version | cut -d " " -f 2 )
+  CHROMIUM_VERSION=$(docker run $BASE_IMAGE chromium-browser --version | cut -d " " -f 2 )
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
   if [[ "$CIRCLE_BRANCH" = "master" ]]
