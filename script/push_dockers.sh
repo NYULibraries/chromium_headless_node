ecc82af55e302255a9f08613e2370361a7f173b1
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-04 13:40:26 -0500

    reverts chromium version method

--- script/push_dockers.sh
+++ script/push_dockers.sh
@@ -7,7 +7,8 @@ do
   BASE_IMAGE=chromium_headless_node:$version
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_latest
   # grabs version number from Chromium installation
-  CHROMIUM_VERSION=$(docker-compose run node_$major_version chromium-browser --version | sed 's/[[:alpha:]|(|[:space:]]//g')
+  CHROMIUM_VERSION=$(docker-compose run node_$major_version chromium-browser --version | cut -d " " -f 2)
+  # CHROMIUM_VERSION=`echo -n $(docker-compose run node_$major_version chromium-browser --version | sed -e 's/[[:alpha:]|(|[:space:]]//g')`
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
   if [[ "$CIRCLE_BRANCH" = "master" ]]
