9759b83b9947ed3ecd6d449f489a686e0a98afb6
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-04 11:56:59 -0500

    Pulls from quay

--- script/pull_dockers.sh
+++ script/pull_dockers.sh
@@ -2,6 +2,6 @@
 
 for version in 6.16.0 8.15.0 10.15.1
 do
-  docker pull nyulibraries/chromium_headless_node:$version-${CIRCLE_BRANCH//\//_} || \
-  docker pull nyulibraries/chromium_headless_node:$version-chromium_latest
+  docker pull quay.io/nyulibraries/chromium_headless_node:$version-${CIRCLE_BRANCH//\//_} || \
+  docker pull quay.io/nyulibraries/chromium_headless_node:$version-chromium_latest
 done
