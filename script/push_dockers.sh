68463975ab9551139d1bbc984fa48bfb44f76f2b
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-04 13:51:47 -0500

    Script cleanup

--- script/push_dockers.sh
+++ script/push_dockers.sh
@@ -2,15 +2,14 @@
 
 for version in 6.16.0 8.15.0 10.15.1
 do
+  BASE_IMAGE=chromium_headless_node:$version
   patch_version=`echo -n $version | sed -e 's/\..$//g'`
   major_version=`echo -n $version | sed -e 's/\.\S*//g'`
-  BASE_IMAGE=chromium_headless_node:$version
+  # grabs version number from Chromium installation
+  CHROMIUM_VERSION=$(docker-compose run node_$major_version chromium-browser --version | cut -d " " -f 2)
 
   # TAG COMMANDS
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_latest
-  # grabs version number from Chromium installation
-  CHROMIUM_VERSION=$(docker-compose run node_$major_version chromium-browser --version | cut -d " " -f 2)
-  # CHROMIUM_VERSION=`echo -n $(docker-compose run node_$major_version chromium-browser --version | sed -e 's/[[:alpha:]|(|[:space:]]//g')`
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
   docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
