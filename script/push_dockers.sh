03ab8f8449e9dfa8d5f3b220246865bff0daddcf
Elliott Grieco <elliott.grieco@nyu.edu>
2019-07-15 13:19:44 -0400

    Improvements to automated upgradable workflow

--- script/push_dockers.sh
+++ script/push_dockers.sh
@@ -6,24 +6,24 @@ do
   patch_version=`echo -n $version | sed -e 's/\..$//g'`
   major_version=`echo -n $version | sed -e 's/\.\S*//g'`
   # grabs version number from Node & Chromium installations, stripping extraneous text
-  NODE_VERSION=$(docker-compose run node_$major_version node --version | sed 's/v//')
-  CHROMIUM_VERSION=$(docker-compose run node_$major_version chromium-browser --version | sed 's/Chromium //')
+  NODE_VERSION=`docker-compose run node_$major_version node --version | sed -e 's/v//'`
+  CHROMIUM_VERSION=`docker-compose run node_$major_version chromium-browser --version | sed -e 's/Chromium //'`
 
   # TAG COMMANDS
-  docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_latest
-  docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
-  docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_latest
+  docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
+  docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
   # PUSH COMMANDS
-  docker push quay.io/nyulibraries/$BASE_IMAGE-chromium_latest
-  docker push quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
-  docker push quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  docker push quay.io/nyulibraries/$NODE_VERSION-chromium_latest
+  docker push quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
+  docker push quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
-  if [[ "$CIRCLE_BRANCH" = "master" ]]
+  if [[ $CIRCLE_BRANCH == master ]]
   then
     # TAG
-    docker tag $BASE_IMAGE quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION
+    docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION
     # PUSH
-    docker push quay.io/nyulibraries/$BASE_IMAGE-chromium_$CHROMIUM_VERSION
+    docker push quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION
   fi
 done
