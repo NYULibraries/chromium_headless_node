cdf0c85a34f3cc135b443e6e0da23173ec71952d
Elliott Grieco <elliott.grieco@nyu.edu>
2019-07-15 13:54:53 -0400

    Restores repo name to push step

--- script/push_dockers.sh
+++ script/push_dockers.sh
@@ -10,20 +10,20 @@ do
   CHROMIUM_VERSION=`docker run alpine/semver semver -c $(docker-compose run node_$major_version chromium-browser --version)`
 
   # TAG COMMANDS
-  docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_latest
-  docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
-  docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  docker tag $BASE_IMAGE quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_latest
+  docker tag $BASE_IMAGE quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
+  docker tag $BASE_IMAGE quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
   # PUSH COMMANDS
-  docker push quay.io/nyulibraries/$NODE_VERSION-chromium_latest
-  docker push quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
-  docker push quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  docker push quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_latest
+  docker push quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}
+  docker push quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_$CHROMIUM_VERSION-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
   if [[ $CIRCLE_BRANCH == master ]]
   then
     # TAG
-    docker tag $BASE_IMAGE quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION
+    docker tag $BASE_IMAGE quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_$CHROMIUM_VERSION
     # PUSH
-    docker push quay.io/nyulibraries/$NODE_VERSION-chromium_$CHROMIUM_VERSION
+    docker push quay.io/nyulibraries/chromium_headless_node:$NODE_VERSION-chromium_$CHROMIUM_VERSION
   fi
 done
