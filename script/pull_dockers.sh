fb0d680579124a3ca6200c42b295a332e7672be3
Elliott Grieco <elliott.grieco@nyu.edu>
2019-07-16 10:27:48 -0400

    Removes pull step so image built fresh

--- script/pull_dockers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -ex
-
-for version in 8 10 12
-do
-  docker pull quay.io/nyulibraries/chromium_headless_node:$version-${CIRCLE_BRANCH//\//_} || \
-  docker pull quay.io/nyulibraries/chromium_headless_node:$version-chromium_latest
-done
